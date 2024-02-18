7142195a0b43755448739fa66a24512b05c327b1
JamzOhJamz <87213748+nsfury@users.noreply.github.com>
2024-02-17 22:29:10 -0700

    Clear out in preparation for rewrite

--- src/mod.ts
+++
@@ -1,8 +0,0 @@
-// Exports
-import * as _ from "./runtime";
-export { ValueWrapper } from "./runtime";
-export const Runtime = new _.Runtime();
-export * from "./logger";
-export * from "./extras";
-// @ts-ignore Set by webpack at bundle time
-export const version = VERSION;
